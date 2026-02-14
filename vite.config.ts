de6cbf0732dc661dccaf62ac113816c425377a79
renatorofilho <renato.rodrigues.alves.filho@gmail.com>
2026-02-14 11:06:09 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,3 @@
-import { jsxLocPlugin } from "@builder.io/vite-plugin-jsx-loc";
 import tailwindcss from "@tailwindcss/vite";
 import react from "@vitejs/plugin-react";
 import path from "node:path";
@@ -6,9 +5,8 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
-    react(), 
-    tailwindcss(), 
-    jsxLocPlugin()
+    react(),
+    tailwindcss()
   ],
   resolve: {
     alias: {
