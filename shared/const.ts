1a6756231e2ce383e8a6c28cdb1588b0700e5430
renatorofilho <renato.rodrigues.alves.filho@gmail.com>
2026-02-14 08:21:12 -0300

    Update const.ts

--- shared/const.ts
+++ shared/const.ts
@@ -2,3 +2,5 @@ export const API_BASE_URL = import.meta.env.VITE_API_URL || '/api';
 export const UNAUTHED_ERR_MSG = 'Unauthorized';
 export const COOKIE_NAME = 'session';
 export const ONE_YEAR_MS = 365 * 24 * 60 * 60 * 1000;
+export const AXIOS_TIMEOUT_MS = 30000;
+export const NOT_ADMIN_ERR_MSG = 'Not an admin';
